461c042aa8b9db1d3faf0e081241b522c8e1820c
Ouzinem <ouzinem@hotmail.com>
2025-03-03 06:01:05 -0500

    Add data annotations for validation in models

--- Data.Models/BlogPost.cs
+++ Data.Models/BlogPost.cs
@@ -1,9 +1,15 @@
-﻿namespace Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models;
 
 public class BlogPost
 {
+   
     public string? Id { get; set; }
+    [Required]
+    [StringLength(100, MinimumLength = 5)] // Max length of 100 characters and min length of 5 characters
     public string Title { get; set; } = string.Empty;
+    [Required]
     public string Text { get; set; } = string.Empty;
     public DateTime PublishDate { get; set; }
     public Category? Category { get; set; }
